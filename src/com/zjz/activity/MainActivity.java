a2dcd7a1c7796ab9dffc81d432e33bae20d59c0c
zhaojizhuang <571130360@qq.com>
2016-05-19 11:10:46 +0800

    支持前后摄像头

--- src/com/zjz/activity/MainActivity.java
+++ src/com/zjz/activity/MainActivity.java
@@ -1,36 +1,84 @@
 package com.zjz.activity;
 
 import com.example.mycamera.R;
+import com.zjz.camera.CameraInterface;
+import com.zjz.camera.CameraInterface.CamOpenOverCallback;
+import com.zjz.camera.preview.CameraTextureView;
+import com.zjz.util.DisplayUtil;
 
 import android.app.Activity;
+import android.graphics.Point;
+import android.graphics.SurfaceTexture;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup.LayoutParams;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements OnClickListener,CamOpenOverCallback{
+	private static final String TAG = "MainActivity";
+	float previewRate = -1f;
+	Button takePicture ;
+	CameraTextureView textureView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		//在线程中开启摄像头
+		Thread openThread = new Thread(){
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				CameraInterface.getInstance().doOpenCamera(MainActivity.this);
+			}
+		};
+		openThread.start();
+		
 		setContentView(R.layout.activity_main);
+		initUI();
+		//初始化摄像头的属性
+		initViewParams();
+		textureView.setAlpha(1.0f);
+		takePicture.setOnClickListener(this);
 	}
+	private void initViewParams(){
+		LayoutParams params = textureView.getLayoutParams();
+		Point p = DisplayUtil.getScreenMetrics(this);
+		params.width = p.x;
+		params.height = p.y;
+		previewRate = DisplayUtil.getScreenRate(this); //默认全屏的比例预览
+		textureView.setLayoutParams(params);
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		//手动设置拍照ImageButton的大小为120dip×120dip,原图片大小是64×64
+//		LayoutParams p2 = textureView.getLayoutParams();
+//		p2.width = DisplayUtil.dip2px(this, 380);
+//		p2.height = DisplayUtil.dip2px(this, 380);;		
+//		textureView.setLayoutParams(p2);	
+
 	}
-
+	private void initUI(){
+		takePicture=(Button) findViewById(R.id.btn);
+		textureView=(CameraTextureView) findViewById(R.id.camera_textureview);
+	}
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.btn:
+			CameraInterface.getInstance().doTakePicture();
+			Log.d(TAG, "点击了拍照");
+			break;
+
+		default:
+			break;
 		}
-		return super.onOptionsItemSelected(item);
 	}
+	@Override
+	public void cameraHasOpened() {
+		// TODO Auto-generated method stub
+		SurfaceTexture surface = textureView._getSurfaceTexture();
+		CameraInterface.getInstance().doStartPreview(surface, previewRate);
+	}
+
 }
--- src/com/zjz/util/FileUtils.java
+++ src/com/zjz/util/FileUtils.java
@@ -1,13 +1,105 @@
 package com.zjz.util;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+import com.zjz.application.MyApplication;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Environment;
+import android.util.Log;
+import android.widget.Toast;
 
 public class FileUtils {
 	private static final String TAG = "FileUtil";
 	private static final File parentPath = Environment.getExternalStorageDirectory();
 	private static String storagePath = "";
 	private static final String DST_FOLDER_NAME = "PlayCamerazjz";
-
+	/**
+	 * 初始化文件保存路径
+	 * @return
+	 */
+	private static String initPath(){
+		if(storagePath.equals("")){
+			storagePath = parentPath.getAbsolutePath()+"/"+DST_FOLDER_NAME;
+			
+			//检查文件夹是否存在，若不存在，则创新
+			File file=new File(storagePath);
+			if (!file.exists()) {
+				file.mkdir();
+			}
+		}
+		return storagePath;
+	}
+	/**保存Bitmap到sdcard
+	 * 命名方式，时间.jpg
+	 * @param b
+	 */
+	public static void saveBitmap(Bitmap b){
+		//初始化文件夹
+		String path=initPath();
+		long dataTake = System.currentTimeMillis();
+		String jpegName = path + "/" + dataTake +".jpg";
+		try {
+			FileOutputStream fileOutputStream=new FileOutputStream(jpegName);
+			BufferedOutputStream bos = new BufferedOutputStream(fileOutputStream);
+			b.compress(Bitmap.CompressFormat.JPEG, 100, bos);
+			bos.flush();
+			bos.close();
+			Log.w(TAG, "saveBitmap成功");
+			Toast.makeText(MyApplication.getContext(), "saveBitmap成功", Toast.LENGTH_LONG).show();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Log.w(TAG, "saveBitmap失败");
+			Toast.makeText(MyApplication.getContext(), "saveBitmap失败", Toast.LENGTH_LONG).show();
+		}
+	}
+	/**
+	 * 通知媒体更新
+	 */
+	public static void scanSdCard(Context context){
+        String file= storagePath;
+        folderScan(context,file);
+    }
+    //扫描通知单个文件 对媒体进行更新
+    private static void fileScan(Context context,String file){
+    	context.sendBroadcast(new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.parse("file://" + file)));
+    	Log.w(TAG, "文件："+file+"更新完毕");
+    }
+    //Intent.ACTION_MEDIA_SCANNER_SCAN_FILE只会扫面文件不会扫描文件夹，所以得罗列出来文件
+    private static void folderScan(Context context,String path){
+        File file = new File(path);
+        //file存在，并且是文件夹
+        if(file.exists() && file.isDirectory()){
+        	//列出文件
+            File[] array = file.listFiles();
+            
+            for(int i=0;i<array.length;i++){
+                File f = array[i];
+                
+                if(f.isFile()){//FILE TYPE
+                    String name = f.getName();
+                    
+                    if(name.endsWith(".mp4") || name.endsWith(".mp3") || name.endsWith(".jpg")){
+                    	//通知更新单个文件
+                        fileScan(context,f.getAbsolutePath());
+                    }
+                }
+                else {//FOLDER TYPE
+                	//递归
+                    folderScan(context,f.getAbsolutePath());
+                }
+            }
+        }
+    }
 }
