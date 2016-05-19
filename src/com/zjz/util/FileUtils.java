8425edaf0f6651b2833aa372e992eb046531fdd8
zhaojizhuang <571130360@qq.com>
2016-05-19 09:16:51 +0800

    asdf

--- src/com/zjz/util/FileUtils.java
+++ src/com/zjz/util/FileUtils.java
@@ -1,5 +1,13 @@
 package com.zjz.util;
 
+import java.io.File;
+
+import android.os.Environment;
+
 public class FileUtils {
+	private static final String TAG = "FileUtil";
+	private static final File parentPath = Environment.getExternalStorageDirectory();
+	private static String storagePath = "";
+	private static final String DST_FOLDER_NAME = "PlayCamerazjz";
 
 }
